99e48300ba42ff10eaa4c1139b2a410ca75eb6c8
monika-fafluch <43880324+monika-fafluch@users.noreply.github.com>
2019-07-15 10:54:41 +0200

    Delete index.js

--- templates/index.js
+++
@@ -1,8 +0,0 @@
- // index page | get a user's name input and store it in localStorage
- document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector("#form").onsubmit = () => {
-        const name = document.querySelector("#name").value;
-        // save name variable to localStorage
-        localStorage.setItem('name', name);
-    };
-});
